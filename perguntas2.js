7833031f3ac4c88e97005a0a5a30b9019888d598
eli21062006 <128488017+eli21062006@users.noreply.github.com>
2024-09-25 19:45:18 -0300

    Add files via upload

---
+++ perguntas2.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'MATEMATICA',
+    'quantos dias tem 1 ANO?',
+    'UM ANO TEM 365 DIAS',
+)
+
+criaCartao(
+    'Geografia',
+    'Qual E a capital da França?',
+    'A capital da França é Paris',
+)
+
+criaCartao(
+    'GEOGRAFIA',
+    'QUAL E A CAPITAL DA ALEMANHA?',
+    'A CAPITAL DA ALEMANHA E BERLIM',
+)
+
+criaCartao(
+    'GEOGRAFIA',
+    'QUAL E A CAPITAL DA ARGENTINA ?',
+    'BUENOS AIRES',
+)
+
+criaCartao(
+    'GEOGRAFIA',
+    'QUANTOS HABITANTES TEM NO BRASIL?',
+    'NO BRASIL TEM 212, 6 MILHOES DE HABITANTES',
+)
